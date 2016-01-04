4fe2b6812ccf415bdb03edecfea6bf3781ee423b
damarcant <damarcant@gmail.com>
2016-01-04 12:08:44 +0100

    Acentos

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO libros (titulo, anio_publicacion) VALUES ('El Pampinoplas', 1979);
 INSERT INTO libros (titulo, anio_publicacion) VALUES ('Los Batautos hacen batautadas', 1981);
-INSERT INTO libros (titulo, anio_publicacion) VALUES ('Zalacaín el aventurero', 1908);
+INSERT INTO libros (titulo, anio_publicacion) VALUES ('Zalacain el aventurero', 1908);
 
 
  
