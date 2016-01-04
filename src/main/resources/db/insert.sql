a9699c5987fcbd0720cea0cc64c868513d99dec5
damarcant <damarcant@gmail.com>
2016-01-04 12:14:43 +0100

    .

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -1,6 +1,3 @@
 INSERT INTO libros (titulo, anio_publicacion) VALUES ('El Pampinoplas', 1979);
 INSERT INTO libros (titulo, anio_publicacion) VALUES ('Los Batautos hacen batautadas', 1981);
 INSERT INTO libros (titulo, anio_publicacion) VALUES ('Zalacain el aventurero', 1908);
-
-
- 
